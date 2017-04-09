5e03fbebe232e63b1b157c1a2573052dcdbf2a76
buhaiovos <oleksandr.buhaiov@gmail.com>
2017-04-09 17:30:33 +0300

    undone AcademicGroup

---
+++ src/main/java/edu/cad/entities/AcademicGroup.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cad.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.IDENTITY;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Олександр
+ */
+@Entity
+@Table(name = "academic_group")
+public class AcademicGroup {
+    
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private int id;
+    
+    @Column(name = "cipher")
+    private String cipher;
+    
+    @Column(name = "budgetary_students")
+    private int budgetaryStudents;
+    
+    @Column(name = "contract_students")
+    private int contractStudents;
+    
+    @Column(name = "start_year")
+    private int startYear;
+    
+    
+    private int idSpecialization;
+    private int idQualification;
+    private int idEducationForm;
+
+    public AcademicGroup() {
+    }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AcademicGroup other = (AcademicGroup) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }    
+}
