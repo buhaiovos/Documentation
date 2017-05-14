4efeb76102e0ac1b0ce8e6ee285cb80154ac2763
buhaiovos <oleksandr.buhaiov@gmail.com>
2017-05-14 22:20:21 +0300

    finished ColumnFactory

--- src/main/java/edu/cad/documentelements/columns/ColumnFactory.java
+++ src/main/java/edu/cad/documentelements/columns/ColumnFactory.java
@@ -1,59 +1,79 @@
 package edu.cad.documentelements.columns;
 
 import edu.cad.daos.HibernateDAO;
+import edu.cad.documentelements.semestercolumns.SemesterColumn;
+import edu.cad.documentelements.semestercolumns.SemesterLabsColumn;
+import edu.cad.documentelements.semestercolumns.SemesterLectionsColumn;
+import edu.cad.documentelements.semestercolumns.SemesterPracticesColumn;
 import edu.cad.entities.ControlDictionary;
 import edu.cad.uils.documentutils.ColumnTokenStringSplitter;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Sheet;
 
 public class ColumnFactory {
     
-    /*public static AbstractColumn createColumn(Cell cell) {
-        if (cell != null) {
-            int rowIndex = cell.getRowIndex();
-            int columnIndex = cell.getColumnIndex();
-            Sheet correspondingSheet = cell.getSheet();
-            String cellContent = null;
-            
-            AbstractColumn column = null;
-            
-            // check cell type to understand if 
-            // we can extract string
-            if (cell.getCellTypeEnum() == CellType.STRING) {
-                cellContent = cell.getStringCellValue();
-            }
-            // if exctracted and it's not null continue
-            if ( (cellContent != null) && (cellContent.contains("#")) ) {
-                ColumnTokenStringSplitter ctss = 
-                        new ColumnTokenStringSplitter(cellContent);
-                
-                column = createColumn(ctss, columnIndex);
-                return column;
-            }
-            else {
-                return null;
-            }        
+    public static AbstractColumn createColumn(Cell cell) {
+        if (cell != null) {             
+            String cellContent = getContentIfCellValid(cell);
+            if (cellContent != null) {
+                return createColumn(new ColumnTokenStringSplitter(cellContent), 
+                                    cell.getColumnIndex());
+            }      
         }  
         return null;
-    }*/
+    }
+    
+    private static String getContentIfCellValid(Cell cell) {
+        
+        if (cell.getCellTypeEnum() == CellType.STRING) {
+            String cellContent = cell.getStringCellValue();
+            if ( (cellContent != null) && (cellContent.contains("#")) ) {
+                return cellContent;
+            }
+        }
+        return null;
+    }
 
     private static AbstractColumn createColumn(ColumnTokenStringSplitter ctss, 
-                                                              int columnNumber) {
+                                                int columnNumber) {
         switch (ctss.getType()) {
             case "cipher":
                 return new CipherColumn(columnNumber);
             case "control":
                 int id = Integer.parseInt(ctss.getFirstNumString());
                 ControlDictionary cd = 
-                        new HibernateDAO<>(ControlDictionary.class)
-                                .get(id);
+                        new HibernateDAO<>(ControlDictionary.class).get(id);
                 return new ControlColumn(columnNumber, cd);
             case "department":
                 return new DepartmentColumn(columnNumber);
             case "ects":
                 return new EctsColumn(columnNumber);
+            case "labs":
+                return new LabsColumn(columnNumber);
+            case "lections":
+                return new LectionsColumn(columnNumber);
+            case "practices":
+                return new PracticesColumn(columnNumber);
+            case "section":
+                return new TitleColumn(columnNumber);
+            case "semester":
+                return new SemesterColumn(columnNumber, 
+                        Integer.parseInt(ctss.getFirstNumString()), // semester
+                        Integer.parseInt(ctss.getSecondNumString())); // weeks
+            case "semlabs":
+                return new SemesterLabsColumn(columnNumber, 
+                        Integer.parseInt(ctss.getFirstNumString()),
+                        Integer.parseInt(ctss.getSecondNumString()));
+            case "semlections":
+                return new SemesterLectionsColumn(columnNumber, 
+                        Integer.parseInt(ctss.getFirstNumString()),
+                        Integer.parseInt(ctss.getSecondNumString()));
+            case "sempractices":
+                return new SemesterPracticesColumn(columnNumber, 
+                        Integer.parseInt(ctss.getFirstNumString()),
+                        Integer.parseInt(ctss.getSecondNumString()));
+            default: 
+                return null;                
         }
     }
-
 }
