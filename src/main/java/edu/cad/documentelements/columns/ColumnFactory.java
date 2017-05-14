03fd5ec0582c8f5f3f6ca16b99b1daf01cb1f891
buhaiovos <oleksandr.buhaiov@gmail.com>
2017-05-14 21:02:31 +0300

    ColumnTokenStringSplitter

--- src/main/java/edu/cad/documentelements/columns/ColumnFactory.java
+++ src/main/java/edu/cad/documentelements/columns/ColumnFactory.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.cad.documentelements.columns;
 
-/**
- *
- * @author Олександр
- */
+import edu.cad.uils.documentutils.ColumnTokenStringSplitter;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Sheet;
+
+/*
+semestercolumn takes colNum, semester, weeks
+semlabs1_18
+*/
+
 public class ColumnFactory {
     
+    public static AbstractColumn createColumn(Cell cell) {
+        if (cell != null) {
+            int rowIndex = cell.getRowIndex();
+            int columnIndex = cell.getColumnIndex();
+            Sheet correspondingSheet = cell.getSheet();
+            String cellContent = null;
+            
+            AbstractColumn column = null;
+            
+            // check cell type to understand if 
+            // we can extract string
+            if (cell.getCellTypeEnum() == CellType.STRING) {
+                cellContent = cell.getStringCellValue();
+            }
+            // if exctracted and it's not null continue
+            if ( (cellContent != null) && (cellContent.contains("#")) ) {
+                ColumnTokenStringSplitter ctss = 
+                        new ColumnTokenStringSplitter(cellContent);
+                
+                column = createColumn();
+            }
+            else {
+                return null;
+            }        
+        }  
+        return null;
+    }
+
 }
