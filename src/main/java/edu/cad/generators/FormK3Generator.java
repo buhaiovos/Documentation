41dc0528ce59f65f1c8ebd9842c8c7c042adfcec
curioushamster <astute.hamster@gmail.com>
2017-05-24 14:49:56 +0300

    K3 generator

--- src/main/java/edu/cad/generators/FormK3Generator.java
+++ src/main/java/edu/cad/generators/FormK3Generator.java
@@ -1,18 +1,9 @@
 package edu.cad.generators;
 
-import edu.cad.daos.HibernateDAO;
-import edu.cad.entities.Curriculum;
-import edu.cad.entities.CurriculumSubject;
 import edu.cad.entities.Department;
 import edu.cad.entities.EducationForm;
-import edu.cad.entities.Subject;
-import edu.cad.entities.Workplan;
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 import org.apache.poi.ss.usermodel.Sheet;
 
 public class FormK3Generator implements IDocumentGenerator{
@@ -31,9 +22,7 @@ public class FormK3Generator implements IDocumentGenerator{
     
     @Override
     public void generate() throws IOException {
-        
-        
-        //subjects.iterator().next().
+        List<K3SubjectEntity> subjects = K3SubjectListCreator.createList(educationForm, source, department, 1);
         
         
     }
